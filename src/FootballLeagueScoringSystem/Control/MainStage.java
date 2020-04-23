a74f44239b6fcfdc4dc7a6993cbbf8665401ae24
QUANHAO-NCU <44420952+QUANHAO-NCU@users.noreply.github.com>
2020-04-23 12:09:38 +0800

    controller add

---
+++ src/FootballLeagueScoringSystem/Control/MainStage.java
@@ -0,0 +1,13 @@
+package FootballLeagueScoringSystem.Control;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainStage extends Application {
+   public Stage mainStage = new Stage();
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    }
+
+}
---
+++ src/FootballLeagueScoringSystem/GameLuancher/Main.java
@@ -0,0 +1,10 @@
+package FootballLeagueScoringSystem.GameLuancher;
+
+import FootballLeagueScoringSystem.Control.MainStage;
+import javafx.application.Application;
+
+public class Main  {
+    public static void main(String[] args) {
+        Application.launch(MainStage.class);
+    }
+}
